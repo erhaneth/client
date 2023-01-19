71bfea710d747df342cdda70c30e68d487123081
“erhaneth” <98863443+erhaneth@users.noreply.github.com>
2023-01-19 14:53:46 -0800

    add widget

--- src/scenes/widgets/UserWidget.jsx
+++ src/scenes/widgets/UserWidget.jsx
@@ -1,10 +1,10 @@
 import {
-    ManageAccountOutlined,
-    EditOutLined,
-    LocationOutlined,
-    WorkOutlined,
+  ManageAccountOutlined,
+  EditOutLined,
+  LocationOutlined,
+  WorkOutlined,
 } from "@mui/icons-material";
-import { Box, Typography, Divider, useTheme} from "@mui/material"
+import { Box, Typography, Divider, useTheme } from "@mui/material";
 
 import UserImage from "components/UserImage";
 import FlexBetween from "components/FlexBetween";
@@ -13,3 +13,47 @@ import { useSelector } from " react-redux";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
+const UserWidget = ({ userId, picturePath }) => {
+  //get user info from backend
+  const [user, setUser] = useState(null);
+  const { palette } = useTheme();
+  const navigate = useNavigate();
+  const token = useSelector((state) => state.token);
+  const dark = palette.neutral.dark;
+  const medium = palette.neutral.medium;
+  const main = palette.neutral.main;
+
+  const getUser = async () => {
+    const response = await fetch(`http://localhost:3001/users/${userId}`, {
+      method: "GET",
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    const data = await response.json();
+    setUser(data);
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []); //eslint-disable-line react-hooks/exhaustive-deps
+  if (!user) {
+    return null;
+  }
+  //destructure user from items
+  const {
+    firstName,
+    lastName,
+    location,
+    occupation,
+    viewedProfile,
+    impressions,
+    friends,
+  } = user;
+
+  return (
+    <WidgetWrapper>
+    {/* FIRST ROW  */}
+
+    </WidgetWrapper>
+
+  )
+};
