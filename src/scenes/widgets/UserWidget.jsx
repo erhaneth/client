18e05470631b0a4e2c1d83b6fce74345b69e8a50
“erhaneth” <98863443+erhaneth@users.noreply.github.com>
2023-01-19 14:10:47 -0800

    add userwidget

---
+++ src/scenes/widgets/UserWidget.jsx
@@ -0,0 +1,15 @@
+import {
+    ManageAccountOutlined,
+    EditOutLined,
+    LocationOutlined,
+    WorkOutlined,
+} from "@mui/icons-material";
+import { Box, Typography, Divider, useTheme} from "@mui/material"
+
+import UserImage from "components/UserImage";
+import FlexBetween from "components/FlexBetween";
+import WidgetWrapper from "components/WidgetWrapper";
+import { useSelector } from " react-redux";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
