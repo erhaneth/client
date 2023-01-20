87e201ef9d4a567a99c81d5292493bafeac2b97a
“erhaneth” <98863443+erhaneth@users.noreply.github.com>
2023-01-19 22:46:46 -0800

    crsuhed the bug

--- src/scenes/widgets/UserWidget.jsx
+++ src/scenes/widgets/UserWidget.jsx
@@ -72,7 +72,8 @@ import {
               >
                 {firstName} {lastName}
               </Typography>
-              <Typography color={medium}>{friends.length} friends</Typography>
+              <Typography color={medium}>{friends.length}
+ friends</Typography>
             </Box>
           </FlexBetween>
           <ManageAccountsOutlined />
