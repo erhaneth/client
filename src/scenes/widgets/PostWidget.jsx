9609496b8aa7b2c4afdc0534f304f6708778e094
“erhaneth” <98863443+erhaneth@users.noreply.github.com>
2023-01-20 13:14:50 -0800

    add post widget

--- src/scenes/widgets/PostWidget.jsx
+++ src/scenes/widgets/PostWidget.jsx
@@ -7,3 +7,23 @@ import {
 import { Box, Divider, IconButton, Typography, useTheme } from "@mui/material";
 import FlexBetween from "components/FlexBetween";
 import Friend from "components/Friend";
+import WidgetWrapper from "components/WidgetWrapper";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setPost } from state;
+
+const PostWidget = ({
+    postId,
+    postUserId,
+    name,
+    description,
+    location,
+    picturePath,
+    userPicturePath,
+    likes,
+    comments, 
+           
+}) => {
+
+}
+export default PostWidget
