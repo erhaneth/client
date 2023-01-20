d47ef5e0e67f50ac879122607c6a3629073c2908
“erhaneth” <98863443+erhaneth@users.noreply.github.com>
2023-01-20 09:58:51 -0800

    add friend widget

---
+++ src/components/Friend.jsx
@@ -0,0 +1,17 @@
+import { PersonAddOutlined, PersonRemoveOutlined } from "@mui/icons-material";
+import { Box, IconButton, Typography, useTheme } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import FlexBetween from "./FlexBetween";
+import UserImage from "./UserImage";
+
+const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { _id } = useSelector((state) => state.user);
+  const token = useSelector((state) => state.token);
+  const friends = useSelector((state) => state.user.friends);
+
+  const { palette } = useTheme();
+};
+export default Friend;
